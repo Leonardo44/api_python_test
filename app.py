69da592bab2d293be18b19ad1b98df2e8fcb6d08
leonarrdo404 <leo@ese.plus>
2024-06-02 16:13:27 -0600

    fix def

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify
 app == Flask(__name__)
 
 @app.route('/api', methods=['GET'])
-def get_data()
+def get_data():
     return jsonify({'message': 'Hello, world!'})
 
 if __name__ == '__main__':
